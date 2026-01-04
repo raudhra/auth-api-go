0f4d7c9e495ad7d5fe86a7131939fa25fd80a0f6
raudhra <rudra888pratap@gmail.com>
2026-01-05 02:25:29 +0530

    added some fucking json token bullshit system. why is t so hard to understand

---
+++ utils/jwt.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func VerifyJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+}
