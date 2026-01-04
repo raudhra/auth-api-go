c12a31dcffde13597ef229a097ab463cdafe7ae6
raudhra <rudra888pratap@gmail.com>
2026-01-05 04:50:02 +0530

    Debugging-3

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,5 +1,6 @@
 package handlers
-import(
+
+import (
 	"encoding/json"
 	"net/http"
 
@@ -7,50 +8,51 @@ import(
 	"go-auth-api/utils"
 )
 
-type Credentials struct{
-	Email string `json:"email"`
+type Credentials struct {
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-func Register(w http.ResponseWriter, r *http.Request){
+func Register(w http.ResponseWriter, r *http.Request) {
 	var creds Credentials
 	json.NewDecoder(r.Body).Decode(&creds)
+
+	hash, _ := utils.HashPassword(creds.Password)
+
+	_, err := db.DB.Exec(
+		"INSERT INTO users (email,password) VALUES ($1,$2)", creds.Email, hash,
+	)
+
+	if err != nil {
+		http.Error(w, "User Exits", 400)
+		return
+	}
+
+	w.Write([]byte("User Registered"))
 }
 
-hash, _ := utils.HashPassword(creds.Password)
-
-_, err := db.DB.Exec(
-	"INSERT INTO users (email,password) VALUES ($1,$2)", creds.Email,hash
-)
-
-if err != nil{
-	http.Error(w, "User Exits", 400)
-	return
-}
-
-w.Write([]byte("User Registered"))
-
-func Login(w http.ResponseWriter, r *http.Request){
-	var cred Credentials
+func Login(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
 	json.NewDecoder(r.Body).Decode(&creds)
 	var id int
 	var hash string
-	err := db.Db.QueryRow(
-		"SELECT id, password FROM user WHERE email=$1", creds.Email
-	).Scan(&id,&hash)
+	err := db.DB.QueryRow(
+		"SELECT id, password FROM user WHERE email=$1", creds.Email,
+	).Scan(&id, &hash)
+
+	if err != nil {
+		http.Error(w, "Invalid Credentials", 401)
+		return
+	}
+	if utils.CheckPassword(hash, creds.Password) != nil {
+		http.Error(w, "Invalid Credentials", 401)
+		return
+	}
+	token, _ := utils.GenerateJWT(id)
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": token,
+	})
 }
-if err!=nil{
-	http.Error(w,"Invalid Credentials", 401)
-	return
-}
-if utils.CheckPassword(hash, creds.Password) != nil{
-	http.Error(w,"Invalid Credentials", 401)
-	return
-}
-token, _ := utils.GenerateJWT(id)
-json.NewEncoder(w).Encode(map[string]string{
-	"token" : token
-})
 func Profile(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Protected profile data"))
 }
